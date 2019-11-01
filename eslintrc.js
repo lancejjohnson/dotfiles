5a26ea24a2252a708244298b742fd0ec3b2ab421
Lance Johnson <lancejjohnson@gmail.com>
2019-11-01 10:41:38 -0400

    Remove airbnb

--- eslintrc.js
+++ eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es6": true
     },
-    "extends": "airbnb",
+    // "extends": "airbnb",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
