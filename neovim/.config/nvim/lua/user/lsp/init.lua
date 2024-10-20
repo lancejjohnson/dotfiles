f8e70b56e51d8154daaa41c054793fc7ecf707d4
Lance Johnson <lancejjohnson@gmail.com>
2024-10-20 14:41:52 -0400

    Get rid of everything

--- neovim/.config/nvim/lua/user/lsp/init.lua
+++
@@ -1,6 +0,0 @@
-require("user.lsp.mason")
-require("user.lsp.handlers").setup()
-require("user.lsp.null-ls")
-
--- https://www.reddit.com/r/neovim/comments/14ecf5o/semantic_highlights_messing_with_todo_comments/
-vim.api.nvim_set_hl(0, '@lsp.type.comment', {})
--- neovim/.config/nvim/lua/user/mini-trailspace.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, trailspace = pcall(require, "mini.trailspace")
-if not status_ok then
-  vim.notify("could not load mini.trailspace")
-  return
-end
-
-trailspace.setup({only_in_normal_buffers = true})
-
