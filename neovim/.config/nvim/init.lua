6ac9c68a00119484c11438371603129da64b9bf8
Lance Johnson <lancejjohnson@gmail.com>
2024-10-22 12:19:43 -0400

    Update cursorline options

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -152,6 +152,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
+vim.opt.cursorlineopt = 'number'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
