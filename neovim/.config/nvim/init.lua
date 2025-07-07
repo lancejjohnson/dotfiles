81a83b8628a0eb06549042f9bd291cc45233a97c
Lance Johnson <lancejjohnson@gmail.com>
2025-07-07 07:29:16 -0400

    Misc

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -866,7 +866,7 @@ require('lazy').setup({
       italic = {
         strings = false,
         emphasis = false,
-        comments = false,
+        comments = true,
         operators = false,
         folds = true,
       },
