0b8cde78640e7258bd6a6abd79487a1e2f9ce5cd
Lance Johnson <lancejjohnson@gmail.com>
2025-09-28 14:49:56 -0400

    Remove direct functions

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -5,51 +5,9 @@ spoon.Hammerflow.loadFirstValidTomlFile {
   'Spoons/Hammerflow.spoon/sample.toml',
 }
 
-local movewindow = function(target)
-  if not target then
-    hs.notify.show('Hammerflow', 'not found', 'unknown target')
-  end
-  local w = hs.window.focusedWindow()
-  w:move(target)
-end
+require 'windows'
 
-local windowFunctions = {
-  ['left25'] = function()
-    movewindow(hs.layout.left25)
-  end,
-  ['left30'] = function()
-    movewindow(hs.layout.left30)
-  end,
-  ['left50'] = function()
-    movewindow(hs.layout.left50)
-  end,
-  ['left70'] = function()
-    movewindow(hs.layout.left70)
-  end,
-  ['left75'] = function()
-    movewindow(hs.layout.left75)
-  end,
-  ['maximized'] = function()
-    movewindow(hs.layout.maximized)
-  end,
-  ['right25'] = function()
-    movewindow(hs.layout.right25)
-  end,
-  ['right30'] = function()
-    movewindow(hs.layout.right30)
-  end,
-  ['right50'] = function()
-    movewindow(hs.layout.right50)
-  end,
-  ['right70'] = function()
-    movewindow(hs.layout.right70)
-  end,
-  ['right75'] = function()
-    movewindow(hs.layout.right75)
-  end,
-}
-
-spoon.Hammerflow.registerFunctions(windowFunctions)
+spoon.Hammerflow.registerFunctions(windows.windowFunctions)
 
 -- NOTE(ljj): I think this is another (prob widely used) spoon
 -- https://github.com/Hammerspoon/Spoons/raw/master/Spoons/ReloadConfiguration.spoon.zip
