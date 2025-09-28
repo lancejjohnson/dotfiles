40eb2cabbf236aa6f58e548246ea24e18c0cd1f0
Lance Johnson <lancejjohnson@gmail.com>
2025-09-28 07:42:24 -0400

    Copy stylua config from neovim kickstart

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -1,37 +1,55 @@
--- Function to swap capture groups in clipboard
+-- Hammerflow
+hs.loadSpoon 'Hammerflow'
+spoon.Hammerflow.loadFirstValidTomlFile {
+  -- TODO(ljj): Define your own toml
+  -- "home.toml",
+  -- "work.toml",
+  'Spoons/Hammerflow.spoon/sample.toml',
+}
+-- NOTE(ljj): I think this is another (prob widely used) spoon
+-- https://github.com/Hammerspoon/Spoons/raw/master/Spoons/ReloadConfiguration.spoon.zip
+-- optionally respect auto_reload setting in the toml config.
+-- if spoon.Hammerflow.auto_reload then
+--   hs.loadSpoon("ReloadConfiguration")
+--   -- set any paths for auto reload
+--   -- spoon.ReloadConfiguration.watch_paths = {hs.configDir, "~/path/to/my/configs/"}
+--   spoon.ReloadConfiguration:start()
+-- end
+
+-- TODO(ljj): Move this to a mermaid.lua file and load.
 local function insertReturnSequence()
   local clipboard = hs.pasteboard.getContents()
   if not clipboard then
-    hs.alert.show("No clipboard content")
+    hs.alert.show 'No clipboard content'
     return
   end
 
-  local pattern = "([%w%.%/%s%_]+)%s*%-%-?>>%s*([%w%.%/%s%_]+)%s*:%s*.*"
+  local pattern = '([%w%.%/%s%_]+)%s*%-%-?>>%s*([%w%.%/%s%_]+)%s*:%s*.*'
   local capture_1, capture_2 = string.match(clipboard, pattern)
 
   if not capture_1 or not capture_2 then
-    hs.alert.show("Did not match on clipboard content")
+    hs.alert.show 'Did not match on clipboard content'
   end
 
   local function trim(s)
-    return s:match "^%s*(.-)%s*$"
+    return s:match '^%s*(.-)%s*$'
   end
 
-  local result = ""
+  local result = ''
   if capture_1 and capture_2 then
-    result = trim(capture_2) .. " -->> " .. trim(capture_1) .. ": ()"
+    result = trim(capture_2) .. ' -->> ' .. trim(capture_1) .. ': ()'
   end
 
   if result ~= clipboard then
     hs.pasteboard.setContents(result)
     hs.timer.doAfter(0.1, function()
-      hs.eventtap.keyStroke({ "cmd" }, "v")
+      hs.eventtap.keyStroke({ 'cmd' }, 'v')
     end)
-    hs.alert.show("Inserted return statement for sequence diagram")
+    hs.alert.show 'Inserted return statement for sequence diagram'
   else
-    hs.alert.show("No matches found")
+    hs.alert.show 'No matches found'
   end
 end
 
 -- Bind to a hotkey (Cmd+Shift+S)
-hs.hotkey.bind({ "cmd", "option", "control" }, "r", insertReturnSequence)
+hs.hotkey.bind({ 'cmd', 'option', 'control' }, 'r', insertReturnSequence)
